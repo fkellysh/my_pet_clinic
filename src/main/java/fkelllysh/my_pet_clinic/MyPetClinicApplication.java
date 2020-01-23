46fee1bd64364e1845c6fe62f48b9597e1082236
Kelly Shackleton <FKELLYSH@jaguarlandrover.com>
2020-01-23 15:54:15 +0000

    Initial commit

---
+++ src/main/java/fkelllysh/my_pet_clinic/MyPetClinicApplication.java
@@ -0,0 +1,13 @@
+package fkelllysh.my_pet_clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fkelllysh/my_pet_clinic/MyPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package fkelllysh.my_pet_clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
