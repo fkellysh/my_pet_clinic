39a2261a21b50d9d370e8d3cc6ba07b5cb9c60b3
fkellysh <fkellysh@jaguarlandrover.com>
2020-01-24 11:19:17 +0000

    Added map based services. Closes #5

---
+++ pet_clinic_data/src/main/java/my_pet_clinic/services/CrudService.java
@@ -0,0 +1,16 @@
+package my_pet_clinic.services;
+
+import java.util.Set;
+
+public interface CrudService<T, ID> {
+
+    Set<T> findAll();
+
+    T findById(ID id);
+
+    T save(ID id, T object);
+
+    void delete(T object);
+
+    void deleteById(ID id);
+}
---
+++ pet_clinic_data/src/main/java/my_pet_clinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,33 @@
+package my_pet_clinic.services.map;
+
+import my_pet_clinic.model.Owner;
+import my_pet_clinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Long id, Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
