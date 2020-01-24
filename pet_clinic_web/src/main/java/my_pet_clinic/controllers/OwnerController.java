15cc12c534f31fc5db13cae1b5bc27d5d021815a
fkellysh <fkellysh@jaguarlandrover.com>
2020-01-24 13:01:34 +0000

    Added index pages and controllers for vets and owners. Closes #12.

--- pet_clinic_web/src/main/java/my_pet_clinic/controllers/OwnerController.java
+++ pet_clinic_web/src/main/java/my_pet_clinic/controllers/OwnerController.java
@@ -1,7 +1,13 @@
 package my_pet_clinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OwnerController {
+
+    @RequestMapping({"owners", "owners/index", "owners/index.html"})
+    public String listOwners() {
+        return "owners/index";
+    }
 }
