b5482fee02d39a2cbd87c4c4f05b5945268b5657
fkellysh <fkellysh@jaguarlandrover.com>
2020-01-28 11:47:35 +0000

    Pet type, Pet JPA added. Closes #42, #43.

---
+++ pet_clinic_data/src/main/java/my_pet_clinic/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,44 @@
+package my_pet_clinic.services.springdatajpa;
+
+import my_pet_clinic.model.Pet;
+import my_pet_clinic.repositories.PetRepository;
+import my_pet_clinic.services.PetService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PetSDJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return petRepository.save(pet);
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
